ef2a7d517a3c513fe56e8df9b13fe612ec403731
patte2004 <62146589+patte2004@users.noreply.github.com>
2024-07-18 15:11:27 +0200

    Update update_magisk.sh

--- usr/magisk/update_magisk.sh
+++ usr/magisk/update_magisk.sh
@@ -9,7 +9,7 @@ ver="$(cat "$DIR/magisk_version" 2>/dev/null || echo -n 'none')"
 if [ "x$1" = "xcanary" ]
 then
 	nver="canary"
-	magisk_link="https://cdn.jsdelivr.net/gh/topjohnwu/magisk-files@e23b98f8a2ba47b5110dd3e2937231c6a42db0b6/app-debug.apk"
+	magisk_link="https://github.com/topjohnwu/magisk-files/raw/${nver}/app-debug.apk"
 elif [ "x$1" = "xalpha" ]
 then
 	nver="alpha"
